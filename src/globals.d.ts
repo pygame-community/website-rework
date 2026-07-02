fe9ddaa24e9b4ef3f6312b9caeb582ea6bb1ed06
Notenlish <71970100+Notenlish@users.noreply.github.com>
2026-07-02 23:04:31 +0300

    featured game can be changed now

---
+++ src/globals.d.ts
@@ -0,0 +1,6 @@
+// ./src/globals.d.ts
+declare module "*.css";
+declare module "@fontsource/*" {}
+declare module "@fontsource-variable/*" {}
+
+// gets rid of "Cannot find module or type declarations for side-effect import of '@fontsource/geist'.ts(2882)" errors
